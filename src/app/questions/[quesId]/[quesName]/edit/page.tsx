b966d3647db1502bd502b3410220a55db720283b
Jas <jas72609@gmail.com>
2025-08-09 18:49:31 +0530

    check2

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,10 +3,17 @@ import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+// Define an interface for the component's props
+interface EditQuesPageProps {
+  params: {
+    quesId: string;
+    quesName: string;
+  };
+}
 
-    return <EditQues question={question} />;
+const Page = async ({ params }: EditQuesPageProps) => {
+  const question = await databases.getDocument(db, questionCollection, params.quesId);
+  return <EditQues question={question} />;
 };
 
 export default Page;
