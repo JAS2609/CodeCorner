0783ab3c36af0defd2488dd30ae4bd766648a9f1
Jas <jas72609@gmail.com>
2025-08-09 18:01:17 +0530

    edit page change

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -2,18 +2,18 @@ import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
-import { QuestionDocument } from "@/components/QuestionCard"; // <-- Your QuestionDocument interface
+import { QuestionDocument } from "@/components/QuestionCard"; 
 
 const Page = async ({
   params,
 }: {
   params: { quesId: string; quesName: string };
 }) => {
-  const doc = (await databases.getDocument(
+  const doc = await databases.getDocument<QuestionDocument>(
     db,
     questionCollection,
     params.quesId
-  )) as QuestionDocument;
+  );
 
   // Ensure it matches QuestionDocument structure
   const question: QuestionDocument = {
