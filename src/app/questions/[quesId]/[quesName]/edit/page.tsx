e5e517555450669d199416ba0bcb0c1dd3c318d7
Jas <jas72609@gmail.com>
2025-08-09 20:44:40 +0530

    check7

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,10 +3,18 @@ import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
 
-    return <EditQues question={question} />;
+interface PageProps {
+  params: {
+    quesId: string;
+    quesName: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const question = await databases.getDocument(db, questionCollection, params.quesId);
+
+  return <EditQues question={question} />;
 };
 
 export default Page;
