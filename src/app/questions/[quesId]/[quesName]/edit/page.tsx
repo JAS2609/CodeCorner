30e1d3f867bcc885fae1f589f093898fdc213c85
Jas <jas72609@gmail.com>
2025-08-09 20:52:39 +0530

    check8

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,18 +1,22 @@
+// src/app/questions/[quesId]/[quesName]/edit/page.tsx
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-
-type CustomPageProps = {
+// Define the type for the component props
+interface PageProps {
   params: {
     quesId: string;
     quesName: string;
   };
-};
+}
 
-const Page = async ({ params }: CustomPageProps) => {
-  const question = await databases.getDocument(db, questionCollection, params.quesId);
+const Page = async (props: PageProps) => {
+  // Destructure the 'params' object inside the function
+  const { quesId } = props.params;
+
+  const question = await databases.getDocument(db, questionCollection, quesId);
 
   return <EditQues question={question} />;
 };
