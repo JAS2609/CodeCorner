faaeb7686ed66f721e85cc03f84f1095d14484cc
Jas <jas72609@gmail.com>
2025-08-10 23:36:08 +0530

    update user question

--- src/app/users/[userId]/[userSlug]/questions/page.tsx
+++ src/app/users/[userId]/[userSlug]/questions/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Pagination from "@/components/Pagination";
 import QuestionCard from "@/components/QuestionCard";
 import { answerCollection, db, questionCollection, voteCollection } from "@/models/name";
@@ -6,65 +8,66 @@ import { UserPrefs } from "@/store/auth";
 import { Query } from "node-appwrite";
 import React from "react";
 
-const Page = async ({
-    params,
-    searchParams,
-}: {
-    params: { userId: string; userSlug: string };
-    searchParams: { page?: string };
-}) => {
-    searchParams.page ||= "1";
+interface PageProps {
+  params: Promise<{ userId: string; userSlug: string }>;
+  searchParams: { page?: string };
+}
 
-    const queries = [
-        Query.equal("authorId", params.userId),
-        Query.orderDesc("$createdAt"),
-        Query.offset((+searchParams.page - 1) * 25),
-        Query.limit(25),
-    ];
+const Page = async ({ params, searchParams }: PageProps) => {
+  const { userId } = await params;
 
-    const questions = await databases.listDocuments(db, questionCollection, queries);
+  searchParams.page ||= "1";
 
-    questions.documents = await Promise.all(
-        questions.documents.map(async ques => {
-            const [author, answers, votes] = await Promise.all([
-                users.get<UserPrefs>(ques.authorId),
-                databases.listDocuments(db, answerCollection, [
-                    Query.equal("questionId", ques.$id),
-                    Query.limit(1), // for optimization
-                ]),
-                databases.listDocuments(db, voteCollection, [
-                    Query.equal("type", "question"),
-                    Query.equal("typeId", ques.$id),
-                    Query.limit(1), // for optimization
-                ]),
-            ]);
+  const queries = [
+    Query.equal("authorId", userId),
+    Query.orderDesc("$createdAt"),
+    Query.offset((+searchParams.page - 1) * 25),
+    Query.limit(25),
+  ];
 
-            return {
-                ...ques,
-                totalAnswers: answers.total,
-                totalVotes: votes.total,
-                author: {
-                    $id: author.$id,
-                    reputation: author.prefs.reputation,
-                    name: author.name,
-                },
-            };
-        })
-    );
+  const questions = await databases.listDocuments(db, questionCollection, queries);
 
-    return (
-        <div className="px-4">
-            <div className="mb-4">
-                <p>{questions.total} questions</p>
-            </div>
-            <div className="mb-4 max-w-3xl space-y-6">
-                {questions.documents.map(ques => (
-                    <QuestionCard key={ques.$id} ques={ques} />
-                ))}
-            </div>
-            <Pagination total={questions.total} limit={25} />
-        </div>
-    );
+  questions.documents = await Promise.all(
+    questions.documents.map(async ques => {
+      const [author, answers, votes] = await Promise.all([
+        users.get<UserPrefs>(ques.authorId),
+        databases.listDocuments(db, answerCollection, [
+          Query.equal("questionId", ques.$id),
+          Query.limit(1), // for optimization
+        ]),
+        databases.listDocuments(db, voteCollection, [
+          Query.equal("type", "question"),
+          Query.equal("typeId", ques.$id),
+          Query.limit(1), // for optimization
+        ]),
+      ]);
+
+      return {
+        ...ques,
+        totalAnswers: answers.total,
+        totalVotes: votes.total,
+        author: {
+          $id: author.$id,
+          reputation: author.prefs.reputation,
+          name: author.name,
+        },
+      };
+    })
+  );
+
+  return (
+    <div className="px-4">
+      <div className="mb-4">
+        <p>{questions.total} questions</p>
+      </div>
+      <div className="mb-4 max-w-3xl space-y-6">
+        {questions.documents.map(ques => (
+          <QuestionCard key={ques.$id} ques={ques} />
+        ))}
+      </div>
+      <Pagination total={questions.total} limit={25} />
+    </div>
+  );
 };
 
 export default Page;
